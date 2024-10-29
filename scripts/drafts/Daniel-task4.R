1d4b3e80dd69bcaceaabd867d4ddb25d1800b4b8
Ledvin <dledvin@outlook.com>
2024-10-29 16:33:48 -0700

    Update Daniel-task4.R

--- scripts/drafts/Daniel-task4.R
+++ scripts/drafts/Daniel-task4.R
@@ -35,28 +35,38 @@ cv_out <- cv.glmnet(x_train,
                     alpha = 1)
 cv_out_df <- tidy(cv_out)
 
-# LASSO estimates
-fit <- glmnet(x_train, y_train, family = 'binomial')
-fit_df <- tidy(fit)
-
-
-
-
 # Fit best lambda
-best_lambdas_chart <- cv_out_df %>% 
+best_lambda <- cv_out_df %>% 
   arrange(lambda) %>% 
-  filter(nzero<10)
-best_lambda <- min(cv_out_df)
+  filter(nzero<10) %>% 
+  slice(1) %>% 
+  pull(lambda)
 final_lasso <- glmnet(x_train,
                       y_train,
                       family = 'binomial',
                       alpha=1,
                       lambda = best_lambda)
 lasso_coefs <- coef(final_lasso)
-lasso_coefs_df <- as.data.frame(as.matrix(lasso_coefs))
+lasso_coefs_df <- as.data.frame(as.matrix(lasso_coefs)) %>% 
+  filter(s0!=0)
 
 
+## Logistic Regression
+#######################
 
+fit <- glm(class ~ suPAR + PTN + CK-MB + MAPK2 + IgD + DERM + aldolase, 
+           data = y_train, 
+           family = 'binomial')
 
+class_metrics <- metric_set(sensitivity, 
+                            specificity, 
+                            accuracy,
+                            roc_auc)
 
+y_train %>%
+  add_predictions(fit, type = 'response') %>%
+  mutate(est = as.factor(pred > 0.5), tr_c = as.factor(class)) %>%
+  class_metrics(estimate = est,
+                truth = tr_c, pred,
+                event_level = 'second')
 
