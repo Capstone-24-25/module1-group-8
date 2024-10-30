f7878211ab38523c0051519d83c57b75f9e5d59e
Ledvin <dledvin@outlook.com>
2024-10-30 13:08:59 -0700

    Update Daniel-task4.R

--- scripts/drafts/Daniel-task4.R
+++ scripts/drafts/Daniel-task4.R
@@ -41,6 +41,7 @@ best_lambda <- cv_out_df %>%
   filter(nzero<10) %>% 
   slice(1) %>% 
   pull(lambda)
+
 final_lasso <- glmnet(x_train,
                       y_train,
                       family = 'binomial',
@@ -50,23 +51,42 @@ lasso_coefs <- coef(final_lasso)
 lasso_coefs_df <- as.data.frame(as.matrix(lasso_coefs)) %>% 
   filter(s0!=0)
 
+proteins_lasso <- lasso_coefs_df %>% 
+  mutate(proteins = rownames(lasso_coefs_df)) %>% 
+  filter(rownames(lasso_coefs_df) != "intercept") %>%
+  slice(-1) %>% 
+  pull(proteins)
+
 
 ## Logistic Regression
 #######################
 
-fit <- glm(class ~ suPAR + PTN + CK-MB + MAPK2 + IgD + DERM + aldolase, 
-           data = y_train, 
+# Pull proteins
+fit_train <- training(partitions) %>% 
+  select(group, any_of(proteins_lasso)) %>% 
+  mutate(class = (group == 'ASD')) %>% 
+  select(-group)
+  
+fit_test <- testing(partitions) %>% 
+  select(group, any_of(proteins_lasso)) %>% 
+  mutate(class = (group == 'ASD')) %>% 
+  select(-group)
+
+# Fit model with training data
+
+fit <- glm(class ~ .,
+           data = fit_train,
            family = 'binomial')
 
+# Evaluate errors on test set
 class_metrics <- metric_set(sensitivity, 
                             specificity, 
                             accuracy,
                             roc_auc)
 
-y_train %>%
+fit_test %>%
   add_predictions(fit, type = 'response') %>%
   mutate(est = as.factor(pred > 0.5), tr_c = as.factor(class)) %>%
   class_metrics(estimate = est,
                 truth = tr_c, pred,
                 event_level = 'second')
-
